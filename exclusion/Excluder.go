78ff5cc14fa2e8ab9af9fc6f4648579b08d7d700
Alex <luigi.taglialatela1996@gmail.com>
2020-10-22 19:06:40 +0200

    pushing unsaved files

--- exclusion/Excluder.go
+++ exclusion/Excluder.go
@@ -1,12 +1,28 @@
-package exclusion;
+/*
+This file is part of ImageAuGomentationCLI.
+
+ImageAuGomentationCLI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+ImageAuGomentationCLI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ImageAuGomentationCLI.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package exclusion
 
 import (
-	"github.com/lootag/ImageAuGomentationCLI/entities";
+	"github.com/lootag/ImageAuGomentationCLI/entities"
 )
 
 type Excluder interface {
-	GetClassesToExclude(exclusionThreshold int, 
+	GetClassesToExclude(exclusionThreshold int,
 		userDefinedExclusions []string,
 		imageNames []string,
-		annotationType entities.AnnotationType) []string;
+		annotationType entities.AnnotationType) []string
 }
--- exclusion/getAnnotationPathsFromImageNamesWorker.go
+++ exclusion/getAnnotationPathsFromImageNamesWorker.go
@@ -1,13 +1,29 @@
-package exclusion;
+/*
+This file is part of ImageAuGomentationCLI.
 
-import(
-	"sync";
+ImageAuGomentationCLI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+ImageAuGomentationCLI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ImageAuGomentationCLI.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package exclusion
+
+import (
+	"sync"
 )
 
-func getAnnotationPathsFromImageNamesWorker(imageName string, 
+func getAnnotationPathsFromImageNamesWorker(imageName string,
 	annotationPaths chan string,
-	preprocessWaitGroup *sync.WaitGroup){
-	defer (*preprocessWaitGroup).Done();
-	annotationPath := "./Annotations/" + imageName[:len(imageName) - 3] + "xml";
-	annotationPaths <- annotationPath;
+	preprocessWaitGroup *sync.WaitGroup) {
+	defer (*preprocessWaitGroup).Done()
+	annotationPath := "./Annotations/" + imageName[:len(imageName)-3] + "xml"
+	annotationPaths <- annotationPath
 }
